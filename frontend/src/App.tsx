0e25291d904a8378db65eb3ba374cc3818165947
abdelilah <abdouajjouqa@gmail.com>
2026-03-24 15:29:48 +0100

    feat: Implement initial user authentication flow with login form, protected routes, and a basic dashboard.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { AuthPage } from './pages/AuthPage'
+import { Dashboard } from './pages/Dashboard'
+import { ProtectedRoute } from './components/ProtectedRoute'
 
 function App() {
   return (
-    <>
-      <AuthPage />
-    </>
+    <Routes>
+      {/* public routes */}
+      <Route path="/" element={<AuthPage />} />
+
+
+      {/* protected routes */}
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
+    </Routes>
   )
 }
 
