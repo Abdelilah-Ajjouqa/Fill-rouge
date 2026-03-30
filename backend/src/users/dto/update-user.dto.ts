93851aee04bbd6deb434549a12c5e6b55c68ecbb
abdelilah <abdouajjouqa@gmail.com>
2026-03-30 13:37:53 +0100

    fix indentation in update user dto

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -3,7 +3,7 @@ import { IsBoolean, IsOptional } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-	@IsBoolean()
-	@IsOptional()
-	isActive?: boolean;
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
 }
